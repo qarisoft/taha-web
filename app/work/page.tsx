e52309d2290a38d540d4419a82cedb5d47a69dfa
salah <cloud.data.sa@gmail.com>
2025-03-04 01:41:39 +0300

    hi

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -8,5 +8,10 @@ import { Card, CardBody, CardFooter } from "@heroui/card";
 import { Image } from "@heroui/image";
 
 export default function BlogPage() {
-  return <App />;
+  return (
+    <div className="">
+      <div className="text-4xl  p-4 mb-4 rounded">أعمالنا</div>
+      <App />
+    </div>
+  );
 }
