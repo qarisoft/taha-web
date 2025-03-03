307d501d6b8cc8b56a87ac21da9eac19eb9413e0
salah <cloud.data.sa@gmail.com>
2025-03-04 02:11:11 +0300

    hi

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath:"/taha-web",
+    output: "export",  // <=== enables static exports
+    reactStrictMode: true,
+};
 
 module.exports = nextConfig;
