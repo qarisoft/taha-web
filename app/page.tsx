6279176ea34aeb97d750445aba84b2108e256b95
salah <cloud.data.sa@gmail.com>
2025-03-04 03:04:59 +0300

    h8

--- app/page.tsx
+++ app/page.tsx
@@ -65,7 +65,7 @@ function Devices() {
     
   </div> */}
 
-      <div className="px-20 py-5 mb-10 bg-[#025d5e] text-white rounded w-fit text-3xl">
+      <div className="px-20 py-5 mb-10 bg-[#025d5e] text-white rounded w-fit text-2xl lg:text-3xl">
         <h1>بيان بالاجهزة المساحية لدى المؤسسة</h1>
       </div>
 
